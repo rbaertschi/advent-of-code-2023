826d5ebe09108c6f96b1d398681b19ac05e01ca8
Roland Bärtschi <roland.baertschi@gmail.com>
2023-12-06 06:46:05 +0100

    solve boat race - day 6 parts 1 and 2

---
+++ src/main/java/ch/ebynaqon/adventofcode23/Day6.java
@@ -0,0 +1,67 @@
+package ch.ebynaqon.adventofcode23;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 7ms -> distance = (7x - x*x) 1mm
+// d(distance)=7-2x=0 -> 7 = 2x -> x = 7 / 2
+public class Day6 {
+    public static List<Race> parse(String input) {
+        String[] lines = input.split("\n");
+        String[] times = lines[0].split(":")[1].trim().split("\\s+");
+        String[] distances = lines[1].split(":")[1].trim().split("\\s+");
+        ArrayList<Race> races = new ArrayList<>();
+        for (int i = 0; i < times.length; i++) {
+            races.add(new Race(Integer.parseInt(times[i]), Integer.parseInt(distances[i])));
+        }
+        return races;
+    }
+
+    public static List<Race> parseWithoutSpaces(String input) {
+        String[] lines = input.split("\n");
+        return List.of(
+                new Race(
+                        Long.parseLong(lines[0].split(":")[1].trim().replaceAll("\\s", "")),
+                        Long.parseLong(lines[1].split(":")[1].trim().replaceAll("\\s", ""))
+                )
+        );
+    }
+
+    public static int solve(String input) {
+        List<Race> races = parse(input);
+        return races.stream().map(Race::winningTimes)
+                .mapToInt(List::size)
+                .reduce(1, (acc, cur) -> acc * cur);
+    }
+
+    public static int solveWithoutSpaces(String input) {
+        List<Race> races = parseWithoutSpaces(input);
+        return races.stream().map(Race::winningTimes)
+                .mapToInt(List::size)
+                .reduce(1, (acc, cur) -> acc * cur);
+    }
+
+    public record Race(long time, long maxDistance) {
+        public List<Long> winningTimes() {
+            var winningTimes = new ArrayList<Long>();
+            var bestTime = time() / 2;
+            for (long i = bestTime; i < time(); i++) {
+                var distance = (time - i) * i;
+                if (distance > maxDistance()) {
+                    winningTimes.add(i);
+                } else {
+                    break;
+                }
+            }
+            for (long i = bestTime - 1; i > 0; i--) {
+                var distance = (time - i) * i;
+                if (distance > maxDistance()) {
+                    winningTimes.add(i);
+                } else {
+                    break;
+                }
+            }
+            return winningTimes;
+        }
+    }
+}
---
+++ src/test/java/ch/ebynaqon/adventofcode23/Day6Test.java
@@ -0,0 +1,78 @@
+package ch.ebynaqon.adventofcode23;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Day6Test {
+    @Test
+    void parseInput() {
+        var input = """
+                Time:      7  15   30
+                Distance:  9  40  200
+                """.trim();
+
+        var actual = Day6.parse(input);
+
+        assertThat(actual).isEqualTo(List.of(
+                new Day6.Race(7,9),
+                new Day6.Race(15,40),
+                new Day6.Race(30,200)
+        ));
+    }
+
+    @Test
+    void winningTimesForFirstExampleRace() {
+        var race = new Day6.Race(7, 9);
+
+        var actual = race.winningTimes();
+
+        assertThat(actual).hasSameElementsAs(List.of(2L, 3L, 4L, 5L));
+    }
+
+    @Test
+    void sloveForExampleInput() throws IOException, URISyntaxException {
+        var input = """
+                Time:      7  15   30
+                Distance:  9  40  200
+                """.trim();
+
+        var actual = Day6.solve(input);
+
+        assertThat(actual).isEqualTo(288);
+    }
+
+    @Test
+    void sloveForExampleInputPart2() throws IOException, URISyntaxException {
+        var input = """
+                Time:      71530
+                Distance:  940200
+                """.trim();
+
+        var actual = Day6.solve(input);
+
+        assertThat(actual).isEqualTo(71503);
+    }
+
+    @Test
+    void sloveForPuzzleInput() throws IOException, URISyntaxException {
+        var input = TestHelper.readInput("/day6-input.txt").trim();
+
+        var actual = Day6.solve(input);
+
+        assertThat(actual).isEqualTo(1731600);
+    }
+
+    @Test
+    void sloveForPuzzleInput2() throws IOException, URISyntaxException {
+        var input = TestHelper.readInput("/day6-input.txt").trim();
+
+        var actual = Day6.solveWithoutSpaces(input);
+
+        assertThat(actual).isEqualTo(40087680);
+    }
+}
