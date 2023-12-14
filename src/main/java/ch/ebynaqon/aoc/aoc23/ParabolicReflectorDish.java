f9ac46c94e5239f465f1fc867b74f8c6284e474f
Roland Bärtschi <roland.baertschi@gmail.com>
2023-12-14 22:00:13 +0100

    calculate load on mirror after a billion tilt cycles - day 14 part 2

--- src/main/java/ch/ebynaqon/aoc/aoc23/ParabolicReflectorDish.java
+++ src/main/java/ch/ebynaqon/aoc/aoc23/ParabolicReflectorDish.java
@@ -1,40 +1,160 @@
 package ch.ebynaqon.aoc.aoc23;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashMap;
 
-public record ParabolicReflectorDish(String rockDistribution) {
-    public static ParabolicReflectorDish parse(String input) {
-        return new ParabolicReflectorDish(input);
+public class ParabolicReflectorDish {
+    private final int rows;
+    private final int columns;
+
+    public boolean[][] rollingRockMap() {
+        return rollingRockMap;
     }
 
-    public long solve() {
+    public boolean[][] blockerMap() {
+        return blockerMap;
+    }
+
+    private final boolean[][] rollingRockMap;
+    private final boolean[][] blockerMap;
+
+    public ParabolicReflectorDish(boolean[][] rollingRockMap, boolean[][] blockerMap) {
+        this.rollingRockMap = rollingRockMap;
+        this.blockerMap = blockerMap;
+        this.rows = rollingRockMap.length;
+        this.columns = rollingRockMap[0].length;
+    }
+
+    public static ParabolicReflectorDish parse(String input) {
+        String[] lines = input.split("\n");
+        int rows = lines.length;
+        int columns = lines[0].length();
+        boolean[][] rollingRockMap = new boolean[rows][columns];
+        boolean[][] blockerMap = new boolean[rows][columns];
+        for (int row = 0; row < rows; row++) {
+            var line = lines[row];
+            for (int column = 0; column < columns; column++) {
+                if (line.charAt(column) == '#') {
+                    blockerMap[row][column] = true;
+                } else if (line.charAt(column) == 'O') {
+                    rollingRockMap[row][column] = true;
+                }
+            }
+        }
+        return new ParabolicReflectorDish(rollingRockMap, blockerMap);
+    }
+
+    public long calculateStressAfterOneTiltNorth() {
+        tiltNorth();
+        return stressOnNorth();
+    }
+
+    private int stressOnNorth() {
         int totalWeight = 0;
-        for (var column : columns()) {
-            int columnLength = column.length();
-            int nextWeight = columnLength;
-            for (int i = 0; i < columnLength; i++) {
-                if (column.charAt(i) == '#') {
-                    nextWeight = columnLength - 1 - i;
-                } else if (column.charAt(i) == 'O') {
-                    totalWeight += nextWeight--;
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                if (rollingRockMap[row][column]) {
+                    totalWeight += rows - row;
                 }
             }
         }
         return totalWeight;
     }
 
-    public List<String> columns() {
-        String[] lines = rockDistribution().split("\n");
-        int numberOfColumns = lines[0].length();
-        var columns = new ArrayList<String>();
-        for (int i = 0; i < numberOfColumns; i++) {
-            var columnBuilder = new StringBuilder(lines.length);
-            for (var line : lines) {
-                columnBuilder.append(line.charAt(i));
+    public int calculateStressAfterABillionCycles(int targetCycles1) {
+        var firstOccurrence = new HashMap<Integer, Integer>();
+        int targetCycles = targetCycles1;
+        for (int currentCycle = 1; currentCycle <= targetCycles; currentCycle++) {
+            cycle();
+            int hash = computeHash(rollingRockMap);
+            if (firstOccurrence.containsKey(hash)) {
+                int lastCycle = firstOccurrence.get(hash);
+                int remainingCycles = targetCycles - currentCycle;
+                int loopLength = currentCycle - lastCycle;
+                int remainingCyclesToCompute = remainingCycles % loopLength;
+                for (int i = 0; i < remainingCyclesToCompute; i++) {
+                    cycle();
+                }
+                return stressOnNorth();
             }
-            columns.add(columnBuilder.toString());
+            firstOccurrence.put(hash, currentCycle);
         }
-        return columns;
+        return stressOnNorth();
     }
+
+    private int computeHash(boolean[][] rollingRockMap) {
+        int hash = 0;
+        for (boolean[] row : rollingRockMap) {
+            hash += 89 * Arrays.hashCode(row);
+        }
+        return hash;
+    }
+
+    private void cycle() {
+        tiltNorth();
+        tiltWest();
+        tiltSouth();
+        tiltEast();
+    }
+
+    private void tiltNorth() {
+        for (int column = 0; column < columns; column++) {
+            int newRow = 0;
+            for (int row = 0; row < rows; row++) {
+                if (blockerMap[row][column]) {
+                    newRow = row + 1;
+                } else if (rollingRockMap[row][column]) {
+                    rollingRockMap[row][column] = false;
+                    rollingRockMap[newRow][column] = true;
+                    newRow = newRow + 1;
+                }
+            }
+        }
+    }
+
+    private void tiltSouth() {
+        for (int column = 0; column < columns; column++) {
+            int newRow = rows - 1;
+            for (int row = rows - 1; row >= 0; row--) {
+                if (blockerMap[row][column]) {
+                    newRow = row - 1;
+                } else if (rollingRockMap[row][column]) {
+                    rollingRockMap[row][column] = false;
+                    rollingRockMap[newRow][column] = true;
+                    newRow = newRow - 1;
+                }
+            }
+        }
+    }
+
+    private void tiltWest() {
+        for (int row = 0; row < rows; row++) {
+            int newColumn = 0;
+            for (int column = 0; column < columns; column++) {
+                if (blockerMap[row][column]) {
+                    newColumn = column + 1;
+                } else if (rollingRockMap[row][column]) {
+                    rollingRockMap[row][column] = false;
+                    rollingRockMap[row][newColumn] = true;
+                    newColumn = newColumn + 1;
+                }
+            }
+        }
+    }
+
+    private void tiltEast() {
+        for (int row = 0; row < rows; row++) {
+            int newColumn = columns - 1;
+            for (int column = columns - 1; column >= 0; column--) {
+                if (blockerMap[row][column]) {
+                    newColumn = column - 1;
+                } else if (rollingRockMap[row][column]) {
+                    rollingRockMap[row][column] = false;
+                    rollingRockMap[row][newColumn] = true;
+                    newColumn = newColumn - 1;
+                }
+            }
+        }
+    }
+
 }
--- src/test/java/ch/ebynaqon/aoc/aoc23/ParabolicReflectorDishTest.java
+++ src/test/java/ch/ebynaqon/aoc/aoc23/ParabolicReflectorDishTest.java
@@ -1,10 +1,7 @@
 package ch.ebynaqon.aoc.aoc23;
 
 import ch.ebynaqon.aoc.aoc23.helper.TestHelper;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,7 +16,16 @@ class ParabolicReflectorDishTest {
 
         var actual = ParabolicReflectorDish.parse(input);
 
-        assertThat(actual).isEqualTo(new ParabolicReflectorDish(input));
+        assertThat(actual.rollingRockMap()).isEqualTo(new boolean[][]{
+                {false, false, false},
+                {true, false, true},
+                {true, true, false}
+        });
+        assertThat(actual.blockerMap()).isEqualTo(new boolean[][]{
+                {false, false, false},
+                {false, true, false},
+                {false, false, false}
+        });
     }
 
     @Test
@@ -30,7 +36,7 @@ class ParabolicReflectorDishTest {
                 OO.
                 """.trim();
 
-        var actual = ParabolicReflectorDish.parse(input).solve();
+        var actual = ParabolicReflectorDish.parse(input).calculateStressAfterOneTiltNorth();
 
         assertThat(actual).isEqualTo(9);
     }
@@ -50,7 +56,7 @@ class ParabolicReflectorDishTest {
                 #OO..#....
                 """.trim();
 
-        var actual = ParabolicReflectorDish.parse(input).solve();
+        var actual = ParabolicReflectorDish.parse(input).calculateStressAfterOneTiltNorth();
 
         assertThat(actual).isEqualTo(136);
     }
@@ -59,8 +65,38 @@ class ParabolicReflectorDishTest {
     void solvePart1() {
         var input = TestHelper.readInput("/day14-input.txt").trim();
 
-        var actual = ParabolicReflectorDish.parse(input).solve();
+        var actual = ParabolicReflectorDish.parse(input).calculateStressAfterOneTiltNorth();
 
         assertThat(actual).isEqualTo(106990);
     }
+
+    @Test
+    void solvePart2() {
+        var input = TestHelper.readInput("/day14-input.txt").trim();
+
+        int b = ParabolicReflectorDish.parse(input).calculateStressAfterABillionCycles(1_000_000_000);
+
+        assertThat(b).isEqualTo(100531);
+    }
+
+    @Test
+    void solveExamplePart2() {
+        var input = """
+                O....#....
+                O.OO#....#
+                .....##...
+                OO.#O....O
+                .O.....O#.
+                O.#..O.#.#
+                ..O..#O..O
+                .......O..
+                #....###..
+                #OO..#....
+                """.trim();
+
+        var actual = ParabolicReflectorDish.parse(input).calculateStressAfterABillionCycles(1_000_000_000);
+
+        assertThat(actual).isEqualTo(64);
+    }
+
 }
