a3d78341c732a04ae15a085703985b805f65e2c5
Roland Bärtschi <roland.baertschi@gmail.com>
2023-12-18 22:11:35 +0100

    calculate dig volume using scan lines - day 18 part 2

--- src/main/java/ch/ebynaqon/aoc/aoc23/LavaductLagoon.java
+++ src/main/java/ch/ebynaqon/aoc/aoc23/LavaductLagoon.java
@@ -1,14 +1,13 @@
 package ch.ebynaqon.aoc.aoc23;
 
-import java.util.*;
-import java.util.function.Predicate;
 
-import static ch.ebynaqon.aoc.aoc23.LavaductLagoon.Direction.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
 
-public class LavaductLagoon {
-
-    private final List<DiggingInstruction> instructions;
-
+public record LavaductLagoon(List<DiggingInstruction> instructions) {
     public static LavaductLagoon parse(String input) {
         var instructions = Arrays.stream(input.split("\n"))
                 .map(DiggingInstruction::parse)
@@ -16,100 +15,69 @@ public class LavaductLagoon {
         return new LavaductLagoon(instructions);
     }
 
-    public LavaductLagoon(List<DiggingInstruction> instructions) {
-        this.instructions = instructions;
-    }
-
-    public List<DiggingInstruction> getInstructions() {
-        return instructions;
-    }
-
-    public int getDigVolume() {
-        int minRow = 0;
-        int maxRow = 0;
-        int minColumn = 0;
-        int maxColumn = 0;
-        Position currentPosition = new Position(0, 0);
-        List<Position> digPath = new ArrayList<>();
-        digPath.add(currentPosition);
-        for (var instruction : instructions) {
-            for (int distance = 1; distance <= instruction.distance(); distance++) {
-                currentPosition = currentPosition.move(instruction.direction());
-                digPath.add(currentPosition);
-                maxColumn = Math.max(maxColumn, currentPosition.column());
-                minColumn = Math.min(minColumn, currentPosition.column());
-                maxRow = Math.max(maxRow, currentPosition.row());
-                minRow = Math.min(minRow, currentPosition.row());
-            }
-        }
-        System.out.printf("Bounding Box (%d,%d) -> (%d,%d)%n", minRow, minColumn, maxRow, maxColumn);
-        digPath = shiftPositions(digPath, minRow, minColumn);
-
-        int rows = maxRow - minRow + 1;
-        int columns = maxColumn - minColumn + 1;
-
-        HashSet<Position> inside = new HashSet<>(digPath);
-        HashSet<Position> outside = new HashSet<>();
-        for (int row = 0; row < rows; row++) {
-            Position position = new Position(row, 0);
-            if (!inside.contains(position)) {
-                outside.add(position);
-            }
-            position = new Position(row, columns - 1);
-            if (!inside.contains(position)) {
-                outside.add(position);
-            }
-        }
-        for (int column = 0; column < columns; column++) {
-            Position position = new Position(0, column);
-            if (!inside.contains(position)) {
-                outside.add(position);
-            }
-            position = new Position(rows - 1, column);
-            if (!inside.contains(position)) {
-                outside.add(position);
-            }
-        }
-
-        Predicate<Position> isValidPosition = position -> position.row() >= 0
-                                                          && position.row() < rows
-                                                          && position.column() >= 0
-                                                          && position.column() < columns;
-
-        Queue<Position> positionsToCheck = new ArrayDeque<>(outside);
-        Position nextPosition = positionsToCheck.poll();
-        while (nextPosition != null) {
-            System.out.println(nextPosition);
-            var positionToCheck = nextPosition.move(UP);
-            if (isValidPosition.test(positionToCheck) && !outside.contains(positionToCheck) && !inside.contains(positionToCheck)) {
-                outside.add(positionToCheck);
-                positionsToCheck.add(positionToCheck);
-            }
-            positionToCheck = nextPosition.move(DOWN);
-            if (isValidPosition.test(positionToCheck) && !outside.contains(positionToCheck) && !inside.contains(positionToCheck)) {
-                outside.add(positionToCheck);
-                positionsToCheck.add(positionToCheck);
-            }
-            positionToCheck = nextPosition.move(LEFT);
-            if (isValidPosition.test(positionToCheck) && !outside.contains(positionToCheck) && !inside.contains(positionToCheck)) {
-                outside.add(positionToCheck);
-                positionsToCheck.add(positionToCheck);
-            }
-            positionToCheck = nextPosition.move(RIGHT);
-            if (isValidPosition.test(positionToCheck) && !outside.contains(positionToCheck) && !inside.contains(positionToCheck)) {
-                outside.add(positionToCheck);
-                positionsToCheck.add(positionToCheck);
-            }
-            nextPosition = positionsToCheck.poll();
-        }
-
-        return rows * columns - outside.size();
-    }
-
-    private static List<Position> shiftPositions(List<Position> digPath, int minRow, int minColumn) {
-        return digPath.stream().map(position ->
-                        new Position(position.row() - minRow, position.column() - minColumn))
+    public static LavaductLagoon parseFromHex(String input) {
+        var instructions = Arrays.stream(input.split("\n"))
+                .map(DiggingInstruction::parseFromHex)
                 .toList();
+        return new LavaductLagoon(instructions);
+    }
+
+    public long getDigVolume() {
+        Position curPosition = new Position(0, 0);
+        List<Line> lines = new ArrayList<>();
+        var yValues = new HashSet<Integer>();
+        yValues.add(curPosition.y());
+        for (int i = 0; i < instructions.size(); i++) {
+            var previousDirection = instructions.get((i + instructions.size() - 1) % instructions.size()).direction();
+            var instruction = instructions.get(i);
+            var nextDirection = instructions.get((i + 1) % instructions.size()).direction();
+            var changesDirection = !previousDirection.equals(nextDirection);
+            Position nextPosition = curPosition.move(instruction.direction, instruction.distance);
+            lines.add(new Line(curPosition, nextPosition, changesDirection));
+            yValues.add(curPosition.y());
+            curPosition = nextPosition;
+        }
+        long volume = 0L;
+        long previousY = Long.MIN_VALUE;
+        var increasingYValues = yValues.stream().sorted().toList();
+        for (var y : increasingYValues) {
+            if (y - 1 > previousY) {
+                volume += getInsideOnLine(y - 1, lines) * (y - 1 - previousY);
+            }
+            volume += getInsideOnLine(y, lines);
+            previousY = y;
+        }
+        return volume;
+    }
+
+    public static long getInsideOnLine(int y, List<Line> lines) {
+        long insideOnLine = 0;
+        long previousX = Long.MIN_VALUE;
+        List<Line> intersectingLines = lines.stream()
+                .filter(line -> line.intersects(y))
+                .sorted(Comparator.comparing(line -> line.from().x()))
+                .toList();
+        boolean isInside = false;
+        boolean previousWasHorizontal = false;
+        for (var line : intersectingLines) {
+            if (isInside) {
+                insideOnLine += Math.max(0, line.minX() - 1 - previousX);
+                if (!line.isHorizontal()) insideOnLine += 1;
+                if (!previousWasHorizontal) insideOnLine += 1;
+            }
+            if (line.isHorizontal()) {
+                insideOnLine += line.length();
+                if (!line.changesDirection()) {
+                    isInside = !isInside;
+                }
+                previousWasHorizontal = true;
+            } else {
+                isInside = !isInside;
+                previousWasHorizontal = false;
+            }
+            previousX = line.maxX();
+        }
+        return insideOnLine;
     }
 
     public enum Direction {
@@ -124,23 +92,67 @@ public class LavaductLagoon {
                 default -> throw new IllegalArgumentException("%s does not represent a direction".formatted(input));
             };
         }
-    }
 
-    public record Position(int row, int column) {
-        public Position move(Direction direction) {
-            return switch (direction) {
-                case UP -> new Position(row() - 1, column());
-                case DOWN -> new Position(row() + 1, column());
-                case LEFT -> new Position(row(), column() - 1);
-                case RIGHT -> new Position(row(), column() + 1);
+        public static Direction parseFromNumber(String input) {
+            return switch (input) {
+                case "3" -> UP;
+                case "1" -> DOWN;
+                case "2" -> LEFT;
+                case "0" -> RIGHT;
+                default -> throw new IllegalArgumentException("%s does not represent a direction".formatted(input));
             };
         }
+    }
+
+    public record Position(int y, int x) {
+
+        public Position move(Direction direction, int distance) {
+            return switch (direction) {
+                case UP -> new Position(y - distance, x);
+                case DOWN -> new Position(y + distance, x);
+                case LEFT -> new Position(y, x - distance);
+                case RIGHT -> new Position(y, x + distance);
+            };
+        }
+    }
+
+    public record Line(Position from, Position to, boolean changesDirection) {
+        public boolean intersects(int y) {
+            return isHorizontal() && from.y == y
+                   || Math.min(from.y, to.y) < y && Math.max(from.y, to.y) > y;
+        }
+
+        public boolean isHorizontal() {
+            return from.y == to.y;
+        }
+
+        public long length() {
+            return isHorizontal()
+                    ? maxX() - minX() + 1
+                    : Math.max(from.y, to.y) - Math.min(from.y, to.y) + 1;
+        }
+
+        private int maxX() {
+            return Math.max(from.x, to.x);
+        }
+
+        private int minX() {
+            return Math.min(from.x, to.x);
+        }
+
     }
 
     public record DiggingInstruction(Direction direction, int distance) {
         static DiggingInstruction parse(String input) {
             String[] parts = input.split("\\s+");
             return new DiggingInstruction(Direction.parse(parts[0]), Integer.parseInt(parts[1]));
+        }
+
+        static DiggingInstruction parseFromHex(String input) {
+            String[] parts = input.split("[()]");
+            Direction direction = Direction.parseFromNumber(parts[1].substring(6));
+            int distance = Integer.parseInt(parts[1].substring(1, 6), 16);
+            return new DiggingInstruction(direction, distance);
         }
     }
 }
--- src/test/java/ch/ebynaqon/aoc/aoc23/LavaductLagoonTest.java
+++ src/test/java/ch/ebynaqon/aoc/aoc23/LavaductLagoonTest.java
@@ -1,12 +1,15 @@
 package ch.ebynaqon.aoc.aoc23;
 
 import ch.ebynaqon.aoc.aoc23.LavaductLagoon.DiggingInstruction;
+import ch.ebynaqon.aoc.aoc23.LavaductLagoon.Line;
+import ch.ebynaqon.aoc.aoc23.LavaductLagoon.Position;
 import ch.ebynaqon.aoc.aoc23.helper.TestHelper;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static ch.ebynaqon.aoc.aoc23.LavaductLagoon.Direction.*;
+import static ch.ebynaqon.aoc.aoc23.LavaductLagoon.getInsideOnLine;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LavaductLagoonTest {
@@ -21,7 +24,7 @@ class LavaductLagoonTest {
 
         var actual = LavaductLagoon.parse(input);
 
-        assertThat(actual.getInstructions()).isEqualTo(List.of(
+        assertThat(actual.instructions()).isEqualTo(List.of(
                 new DiggingInstruction(DOWN, 6),
                 new DiggingInstruction(LEFT, 5),
                 new DiggingInstruction(UP, 2),
@@ -30,7 +33,26 @@ class LavaductLagoonTest {
     }
 
     @Test
-    void solveExample() {
+    void parseFromHex() {
+        var input = """
+                D 6 (#800001)
+                L 5 (#0b0002)
+                U 2 (#005003)
+                R 2 (#000300)
+                """.trim();
+
+        var actual = LavaductLagoon.parseFromHex(input);
+
+        assertThat(actual.instructions()).isEqualTo(List.of(
+                new DiggingInstruction(DOWN, 8 * 16 * 16 * 16 * 16),
+                new DiggingInstruction(LEFT, 11 * 16 * 16 * 16),
+                new DiggingInstruction(UP, 5 * 16 * 16),
+                new DiggingInstruction(RIGHT, 3 * 16)
+        ));
+    }
+
+    @Test
+    void getDigVolumeForExample() {
         var input = """
                 R 6 (#70c710)
                 D 5 (#0dc571)
@@ -54,11 +76,152 @@ class LavaductLagoonTest {
     }
 
     @Test
-    void solvePart1() {
+    void getDigVolumeForSimpleRectangle() {
+        var input = """
+                U 10 (#70c710)
+                R 2 (#0dc571)
+                D 10 (#5713f0)
+                L 2 (#d2c081)
+                """.trim();
+
+        var actual = LavaductLagoon.parse(input).getDigVolume();
+
+        assertThat(actual).isEqualTo(33);
+    }
+
+    @Test
+    void getDigVolumeForStair() {
+        /*
+          XXX
+          XXX
+        XXXXX
+        XXXXX
+        XXXXX
+
+        */
+        var input = """
+                U 2 (#70c710)
+                R 2 (#0dc571)
+                U 2 (#70c710)
+                R 2 (#0dc571)
+                D 4 (#5713f0)
+                L 4 (#d2c081)
+                """.trim();
+
+        var actual = LavaductLagoon.parse(input).getDigVolume();
+
+        assertThat(actual).isEqualTo(21);
+    }
+
+    @Test
+    void getDigVolumeForTetrisLikeShape() {
+        /*
+           XXXXX
+           XXXXX
+        XXXXXXXX
+        XXXXXXXX
+        XXX  XXX
+        XXX
+        */
+        var input = """
+                U 3 (#70c710)
+                R 3 (#0dc571)
+                U 2 (#70c710)
+                R 4 (#0dc571)
+                D 4 (#5713f0)
+                L 2 (#d2c081)
+                U 1 (#d2c081)
+                L 3 (#d2c081)
+                D 2 (#d2c081)
+                L 2 (#d2c081)
+                """.trim();
+
+        var actual = LavaductLagoon.parse(input).getDigVolume();
+
+        assertThat(actual).isEqualTo(35);
+    }
+
+    @Test
+    void getDigVolumeForPart1() {
         var input = TestHelper.readInput("/day17-lavaduct-dig-plan.txt").trim();
 
         var actual = LavaductLagoon.parse(input).getDigVolume();
 
         assertThat(actual).isEqualTo(76387);
+    }
+
+    @Test
+    void getDigVolumeForPart2() {
+        var input = TestHelper.readInput("/day17-lavaduct-dig-plan.txt").trim();
+
+        var actual = LavaductLagoon.parseFromHex(input).getDigVolume();
+
+        assertThat(actual).isEqualTo(250022188522074L);
+    }
+
+    @Test
+    void horizontalLineIntersectsOnMatchingY() {
+        Line line = new Line(new Position(0, 0), new Position(0, 1), false);
+
+        assertThat(line.intersects(0)).isTrue();
+    }
+
+    @Test
+    void verticalLineIntersectsBetweenStartAndEndButNotAtStartAndEnd() {
+        Line line = new Line(new Position(-1, 0), new Position(1, 0), false);
+
+        assertThat(line.intersects(-1)).isFalse();
+        assertThat(line.intersects(0)).isTrue();
+        assertThat(line.intersects(1)).isFalse();
+    }
+
+    @Test
+    void insideCountHorizontalLineOnly() {
+        var lines = List.of(
+                new Line(new Position(0, 0), new Position(0, 10), true)
+        );
+
+        assertThat(getInsideOnLine(0, lines)).isEqualTo(11);
+    }
+
+    @Test
+    void insideCountHorizontalAndVerticalLine() {
+        var lines = List.of(
+                new Line(new Position(0, 0), new Position(0, 10), false),
+                new Line(new Position(10, 20), new Position(-10, 20), false)
+        );
+
+        assertThat(getInsideOnLine(0, lines)).isEqualTo(21);
+    }
+
+    @Test
+    void insideCountVerticalAndHorizontalLine() {
+        var lines = List.of(
+                new Line(new Position(10, 0), new Position(-10, 0), false),
+                new Line(new Position(0, 10), new Position(0, 20), false)
+        );
+
+        assertThat(getInsideOnLine(0, lines)).isEqualTo(21);
+    }
+
+    @Test
+    void insideCountHorizontalBetweenVerticals() {
+        var lines = List.of(
+                new Line(new Position(-10, 0), new Position(10, 0), false),
+                new Line(new Position(0, 10), new Position(0, 20), true),
+                new Line(new Position(10, 30), new Position(-10, 30), false)
+        );
+
+        assertThat(getInsideOnLine(0, lines)).isEqualTo(31);
+    }
+
+    @Test
+    void insideCountWithTwoHorizontalLines() {
+        var lines = List.of(
+                new Line(new Position(0, 0), new Position(0, 10), false),
+                new Line(new Position(0, 20), new Position(0, 30), false)
+        );
+
+        assertThat(getInsideOnLine(0, lines)).isEqualTo(31);
     }
 }
