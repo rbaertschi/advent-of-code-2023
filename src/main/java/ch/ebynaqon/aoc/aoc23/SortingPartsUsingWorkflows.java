3e95db6b71ada68d8d8c6e8f50a07c13d98504d4
Roland Bärtschi <roland.baertschi@gmail.com>
2023-12-19 07:57:56 +0100

    calculate number of accepted parts by mapping value ranges - day 19 part 2

--- src/main/java/ch/ebynaqon/aoc/aoc23/SortingPartsUsingWorkflows.java
+++ src/main/java/ch/ebynaqon/aoc/aoc23/SortingPartsUsingWorkflows.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.function.Function;
 
 public record SortingPartsUsingWorkflows(List<Workflow> workflows, List<Part> parts) {
     public static SortingPartsUsingWorkflows parse(String input) {
@@ -44,11 +43,37 @@ public record SortingPartsUsingWorkflows(List<Workflow> workflows, List<Part> pa
         return getScore(acceptedParts);
     }
 
+    public long getNumberOfAcceptedPartValues() {
+        var acceptedRanges = new ArrayList<PartRange>();
+        var rangesToCheck = new ArrayDeque<PartRange>();
+        var nextRange = new PartRange(new ValueRange(1, 4000), new ValueRange(1, 4000), new ValueRange(1, 4000), new ValueRange(1, 4000), "in");
+        var workflowsByLabel = new HashMap<String, Workflow>();
+        for (var workflow : workflows()) {
+            workflowsByLabel.put(workflow.label(), workflow);
+        }
+        while (nextRange != null) {
+            var workflow = workflowsByLabel.get(nextRange.workflow());
+            List<PartRange> mappedRanges = workflow.mapRange(nextRange);
+            for (var mappedRange : mappedRanges) {
+                if (mappedRange.workflow().equals("A")) {
+                    acceptedRanges.add(mappedRange);
+                } else if (!mappedRange.workflow().equals("R")) {
+                    rangesToCheck.add(mappedRange);
+                }
+            }
+            nextRange = rangesToCheck.poll();
+        }
+        return acceptedRanges.stream().mapToLong(PartRange::size).sum();
+    }
+
     private int getScore(List<Part> acceptedParts) {
         return acceptedParts.stream().mapToInt(Part::score).sum();
     }
 
     record PartAndWorkflow(Part part, String workflow) {
+    }
+
+    public record PartRangeMappingResult(PartRange mapped, PartRange remaining) {
     }
 
     public record Workflow(String label, List<Rule> rules) {
@@ -60,12 +85,30 @@ public record SortingPartsUsingWorkflows(List<Workflow> workflows, List<Part> pa
                     .toList();
             return new Workflow(labelAndRules[0], rules);
         }
+
+        public List<PartRange> mapRange(PartRange range) {
+            var mappedRanges = new ArrayList<PartRange>();
+            var remainingRange = range;
+            for (var rule : rules()) {
+                var result = rule.mapRange(remainingRange);
+                if (result.mapped() != null) {
+                    mappedRanges.add(result.mapped());
+                }
+                remainingRange = result.remaining();
+                if (remainingRange == null) {
+                    break;
+                }
+            }
+            return mappedRanges;
+        }
     }
 
     public interface Rule {
         boolean accepts(Part part);
 
         String targetWorkflow();
+
+        PartRangeMappingResult mapRange(PartRange range);
 
         static Rule parse(String input) {
             String[] conditionAndTargetWorkflow = input.split(":");
@@ -75,32 +118,99 @@ public record SortingPartsUsingWorkflows(List<Workflow> workflows, List<Part> pa
             } else {
                 String condition = conditionAndTargetWorkflow[0];
                 String[] propertyAndThreshold = condition.split("[<>]");
-                Function<Part, Integer> extractor = switch (propertyAndThreshold[0]) {
-                    case "x" -> Part.PROPERTY_X;
-                    case "m" -> Part.PROPERTY_M;
-                    case "a" -> Part.PROPERTY_A;
-                    default -> Part.PROPERTY_S;
-                };
+                String property = propertyAndThreshold[0];
                 int threshold = Integer.parseInt(propertyAndThreshold[1]);
                 if (condition.contains(">")) {
-                    return new MoreThan(extractor, threshold, targetWorkflow);
+                    return new MoreThan(property, threshold, targetWorkflow);
                 } else {
-                    return new LessThan(extractor, threshold, targetWorkflow);
+                    return new LessThan(property, threshold, targetWorkflow);
                 }
             }
         }
 
-        record LessThan(Function<Part, Integer> extractor, int threshold, String targetWorkflow) implements Rule {
-            @Override
-            public boolean accepts(Part part) {
-                return extractor.apply(part) < threshold;
-            }
+        default Integer extractValue(Part part, String property) {
+            return switch (property) {
+                case "x" -> part.x();
+                case "m" -> part.m();
+                case "a" -> part.a();
+                default -> part.s();
+            };
         }
 
-        record MoreThan(Function<Part, Integer> extractor, int threshold, String targetWorkflow) implements Rule {
+        default ValueRange extractRange(PartRange part, String property) {
+            return switch (property) {
+                case "x" -> part.x();
+                case "m" -> part.m();
+                case "a" -> part.a();
+                default -> part.s();
+            };
+        }
+
+        default PartRangeMappingResult splitRange(PartRange range, ValueRange mappedRange, ValueRange unmappedRange, String property, String workflow) {
+            return switch (property) {
+                case "x" -> new PartRangeMappingResult(
+                        new PartRange(mappedRange, range.m(), range.a(), range.s(), workflow),
+                        new PartRange(unmappedRange, range.m(), range.a(), range.s(), range.workflow())
+                );
+                case "m" -> new PartRangeMappingResult(
+                        new PartRange(range.x(), mappedRange, range.a(), range.s(), workflow),
+                        new PartRange(range.x(), unmappedRange, range.a(), range.s(), range.workflow())
+                );
+                case "a" -> new PartRangeMappingResult(
+                        new PartRange(range.x(), range.m(), mappedRange, range.s(), workflow),
+                        new PartRange(range.x(), range.m(), unmappedRange, range.s(), range.workflow())
+                );
+                default -> new PartRangeMappingResult(
+                        new PartRange(range.x(), range.m(), range.a(), mappedRange, workflow),
+                        new PartRange(range.x(), range.m(), range.a(), unmappedRange, range.workflow())
+                );
+            };
+        }
+
+        record LessThan(String property, int threshold, String targetWorkflow) implements Rule {
             @Override
             public boolean accepts(Part part) {
-                return extractor.apply(part) > threshold;
+                return extractValue(part, this.property()) < threshold;
+            }
+
+            @Override
+            public PartRangeMappingResult mapRange(PartRange range) {
+                ValueRange valueRange = extractRange(range, property());
+                if (valueRange.to() < threshold) {
+                    return new PartRangeMappingResult(
+                            new PartRange(range.x(), range.m(), range.a(), range.s(), targetWorkflow()),
+                            null
+                    );
+                } else if (valueRange.from() >= threshold) {
+                    return new PartRangeMappingResult(null, range);
+                }
+                ValueRange mappedRange = new ValueRange(valueRange.from(), threshold - 1);
+                ValueRange unmappedRange = new ValueRange(threshold, valueRange.to());
+                return splitRange(range, mappedRange, unmappedRange, property(), targetWorkflow());
+            }
+
+        }
+
+        record MoreThan(String property, int threshold, String targetWorkflow) implements Rule {
+            @Override
+            public boolean accepts(Part part) {
+                return extractValue(part, this.property()) > threshold;
+            }
+
+            @Override
+            public PartRangeMappingResult mapRange(PartRange range) {
+                ValueRange valueRange = extractRange(range, property());
+                if (valueRange.from() > threshold) {
+                    return new PartRangeMappingResult(
+                            new PartRange(range.x(), range.m(), range.a(), range.s(), targetWorkflow()),
+                            null
+                    );
+                } else if (valueRange.to() <= threshold) {
+                    return new PartRangeMappingResult(null, range);
+                }
+                ValueRange mappedRange = new ValueRange(threshold + 1, valueRange.to());
+                ValueRange unmappedRange = new ValueRange(valueRange.from(), threshold);
+                return splitRange(range, mappedRange, unmappedRange, property(), targetWorkflow());
             }
         }
 
@@ -109,15 +219,18 @@ public record SortingPartsUsingWorkflows(List<Workflow> workflows, List<Part> pa
             public boolean accepts(Part part) {
                 return true;
             }
+
+            @Override
+            public PartRangeMappingResult mapRange(PartRange range) {
+                return new PartRangeMappingResult(
+                        new PartRange(range.x(), range.m(), range.a(), range.s(), targetWorkflow()),
+                        null
+                );
+            }
         }
     }
 
     public record Part(int x, int m, int a, int s) {
-        public static Function<Part, Integer> PROPERTY_X = Part::x;
-        public static Function<Part, Integer> PROPERTY_M = Part::m;
-        public static Function<Part, Integer> PROPERTY_A = Part::a;
-        public static Function<Part, Integer> PROPERTY_S = Part::s;
-
         public static Part parse(String input) {
             // {x=787,m=2655,a=1222,s=2876}
             int x = 0;
@@ -139,6 +252,18 @@ public record SortingPartsUsingWorkflows(List<Workflow> workflows, List<Part> pa
 
         public int score() {
             return x + m + a + s;
+        }
+    }
+
+    public record PartRange(ValueRange x, ValueRange m, ValueRange a, ValueRange s, String workflow) {
+        public long size() {
+            return x.size() * m.size() * a.size() * s.size();
+        }
+    }
+
+    public record ValueRange(int from, int to) {
+        public long size() {
+            return to - from + 1;
         }
     }
 }
--- src/test/java/ch/ebynaqon/aoc/aoc23/SortingPartsUsingWorkflowsTest.java
+++ src/test/java/ch/ebynaqon/aoc/aoc23/SortingPartsUsingWorkflowsTest.java
@@ -26,12 +26,12 @@ class SortingPartsUsingWorkflowsTest {
         assertThat(actual).isEqualTo(new SortingPartsUsingWorkflows(
                 List.of(
                         new Workflow("px", List.of(
-                                new Rule.LessThan(Part.PROPERTY_A, 2006, "qkq"),
-                                new Rule.MoreThan(Part.PROPERTY_M, 2090, "A"),
+                                new Rule.LessThan("a", 2006, "qkq"),
+                                new Rule.MoreThan("m", 2090, "A"),
                                 new Rule.Default("rfg")
                         )),
                         new Workflow("pv", List.of(
-                                new Rule.MoreThan(Part.PROPERTY_A, 1716, "R"),
+                                new Rule.MoreThan("a", 1716, "R"),
                                 new Rule.Default("A")
                         ))
                 ),
@@ -70,11 +70,47 @@ class SortingPartsUsingWorkflowsTest {
     }
 
     @Test
+    void getNumberOfAcceptedPartValuesForExample() {
+        var input = """
+                px{a<2006:qkq,m>2090:A,rfg}
+                pv{a>1716:R,A}
+                lnx{m>1548:A,A}
+                rfg{s<537:gd,x>2440:R,A}
+                qs{s>3448:A,lnx}
+                qkq{x<1416:A,crn}
+                crn{x>2662:A,R}
+                in{s<1351:px,qqz}
+                qqz{s>2770:qs,m<1801:hdj,R}
+                gd{a>3333:R,R}
+                hdj{m>838:A,pv}
+                                
+                {x=787,m=2655,a=1222,s=2876}
+                {x=1679,m=44,a=2067,s=496}
+                {x=2036,m=264,a=79,s=2244}
+                {x=2461,m=1339,a=466,s=291}
+                {x=2127,m=1623,a=2188,s=1013}
+                """.trim();
+
+        var actual = SortingPartsUsingWorkflows.parse(input).getNumberOfAcceptedPartValues();
+
+        assertThat(actual).isEqualTo(167409079868000L);
+    }
+
+    @Test
     void getScoreOfAcceptedPartsForPart1() {
         var input = TestHelper.readInput("/day19-workflows-and-parts.txt").trim();
 
         var actual = SortingPartsUsingWorkflows.parse(input).getScoreOfAcceptedParts();
 
         assertThat(actual).isEqualTo(409898);
+    }
+
+    @Test
+    void getNumberOfAcceptedPartValuesForPart2() {
+        var input = TestHelper.readInput("/day19-workflows-and-parts.txt").trim();
+
+        var actual = SortingPartsUsingWorkflows.parse(input).getNumberOfAcceptedPartValues();
+
+        assertThat(actual).isEqualTo(113057405770956L);
     }
 }
