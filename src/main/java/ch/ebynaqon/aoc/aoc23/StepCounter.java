a2b62bb3c9f712855f202db1a893e6d1c8c7c7d8
Roland Bärtschi <roland.baertschi@gmail.com>
2023-12-22 06:01:26 +0100

    solution for day 21 part 2

--- src/main/java/ch/ebynaqon/aoc/aoc23/StepCounter.java
+++ src/main/java/ch/ebynaqon/aoc/aoc23/StepCounter.java
@@ -1,26 +1,109 @@
 package ch.ebynaqon.aoc.aoc23;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class StepCounter {
-    public static int numberOfReachablePlots(String input, int stepGoal) {
-        String[] lines = input.split("\n");
-        int rows = lines.length;
-        int columns = lines[0].length();
+
+    private final String[] lines;
+    private final int rows;
+    private final int columns;
+    private final HashMap<CacheKey, Integer> cache;
+
+    public StepCounter(String input) {
+        lines = input.split("\n");
+        rows = lines.length;
+        columns = lines[0].length();
+        cache = new HashMap<>();
+    }
+
+    public long numberOfReachablePlotsInInfiniteGarden(int stepGoal) {
+        long totalReachable = 0L;
+        int gardenOffset = Math.max(0, stepGoal - 65) / 131 + (Math.max(0, stepGoal - 65) % 131 > 0 ? 1 : 0);
+        for (int gardenRow = -gardenOffset; gardenRow <= gardenOffset; gardenRow++) {
+            int gardenColumnOffset = gardenOffset - Math.abs(gardenRow);
+            for (int gardenColumn = gardenColumnOffset + 1; gardenColumn > Math.max(0, gardenColumnOffset - 2); gardenColumn--) {
+                totalReachable += getReachableInGarden(stepGoal, new Garden(gardenRow, -gardenColumn));
+                totalReachable += getReachableInGarden(stepGoal, new Garden(gardenRow, gardenColumn));
+            }
+            var remainingColumns = gardenColumnOffset - 2;
+            // multiples of 2 in repeating space
+            if (remainingColumns % 2 == 1) {
+                totalReachable += getReachableInGarden(stepGoal, new Garden(gardenRow, -remainingColumns));
+                totalReachable += getReachableInGarden(stepGoal, new Garden(gardenRow, remainingColumns));
+                remainingColumns--;
+            }
+            if (remainingColumns >= 2) {
+                long multiplier = remainingColumns / 2;
+                totalReachable += multiplier * getReachableInGarden(stepGoal, new Garden(gardenRow, -1));
+                totalReachable += multiplier * getReachableInGarden(stepGoal, new Garden(gardenRow, 1));
+                totalReachable += multiplier * getReachableInGarden(stepGoal, new Garden(gardenRow, -2));
+                totalReachable += multiplier * getReachableInGarden(stepGoal, new Garden(gardenRow, 2));
+            }
+            // center
+            totalReachable += getReachableInGarden(stepGoal, new Garden(gardenRow, 0));
+        }
+        return totalReachable;
+    }
+
+    private int getReachableInGarden(int stepGoal, Garden garden) {
+        int reachableInGarden;
+        int remainingDistance = stepGoal - garden.distanceFromStart();
+        int distanceInGarden = Math.min(2 * 131, remainingDistance);
+        if (distanceInGarden < 0) {
+            return 0;
+        }
+        if (distanceInGarden == 0) {
+            return 1;
+        }
+        Plot startingPlot = garden.startingPlot();
+//        boolean isEven = garden.isEven() ^ (distanceInGarden % 2 == 0);
+        boolean isEven = remainingDistance % 2 == 0;
+        CacheKey key = new CacheKey(startingPlot, distanceInGarden, isEven);
+        if (cache.containsKey(key)) {
+            reachableInGarden = cache.get(key);
+        } else {
+            reachableInGarden = numberOfReachablePlots(distanceInGarden, startingPlot, isEven, false);
+            cache.put(key, reachableInGarden);
+        }
+        return reachableInGarden;
+    }
+
+    public record CacheKey(Plot startingPlot, int numberOfSteps, boolean isEven) {
+    }
+
+    public record Garden(int row, int column) {
+        public boolean isEven() {
+            return (Math.abs(row()) + Math.abs(column())) % 2 == 0;
+        }
+
+        public Plot startingPlot() {
+            return new Plot(
+                    row < 0 ? 130 : row == 0 ? 65 : 0,
+                    column < 0 ? 130 : column == 0 ? 65 : 0
+            );
+        }
+
+        public int distanceFromStart() {
+            int rowDistance = Math.abs(row) > 0 ? 66 + (Math.abs(row) - 1) * 131 : 0;
+            int columnDistance = Math.abs(column) > 0 ? 66 + (Math.abs(column) - 1) * 131 : 0;
+            return rowDistance + columnDistance;
+        }
+    }
+
+    public int numberOfReachablePlots(int stepGoal, Plot startingPlot, boolean isEven, boolean infinite) {
         var plotsReachedWithEvenSteps = new HashSet<Plot>();
         var plotsReachedWithOddSteps = new HashSet<Plot>();
         var plotsToCheck = new HashSet<Plot>();
-        var startingPlot = findStartingPlot(lines);
         plotsReachedWithEvenSteps.add(startingPlot);
         plotsToCheck.add(startingPlot);
         for (int step = 1; step <= stepGoal; step++) {
             var nextPlotsToCheck = new HashSet<Plot>();
             var isEvenStep = (step % 2) == 0;
             for (var plot : plotsToCheck) {
-                for (var neighbourPlot : plot.getNeighboursWithinBounds(rows, columns)) {
-                    if (lines[neighbourPlot.row()].charAt(neighbourPlot.column()) != '#') {
+                for (var neighbourPlot : infinite ? plot.getNeighbours() : plot.getNeighboursWithinBounds(rows, columns)) {
+                    if (lines[(neighbourPlot.row() % rows + rows) % rows].charAt((neighbourPlot.column() % columns + columns) % columns) != '#') {
                         if (isEvenStep && !plotsReachedWithEvenSteps.contains(neighbourPlot)) {
                             plotsReachedWithEvenSteps.add(neighbourPlot);
                             nextPlotsToCheck.add(neighbourPlot);
@@ -34,10 +117,11 @@ public class StepCounter {
             }
             plotsToCheck = nextPlotsToCheck;
         }
-        return plotsReachedWithEvenSteps.size();
+        return isEven ? plotsReachedWithEvenSteps.size() : plotsReachedWithOddSteps.size();
     }
 
-    private static Plot findStartingPlot(String[] lines) {
+    public static Plot findStartingPlot(String input) {
+        String[] lines = input.split("\n");
         for (int row = 0; row < lines.length; row++) {
             var line = lines[row];
             for (int column = 0; column < line.length(); column++) {
@@ -51,14 +135,19 @@ public class StepCounter {
 
     public record Plot(int row, int column) {
         public List<Plot> getNeighboursWithinBounds(int rows, int columns) {
-            return Stream.of(
-                            new Plot(row() + 1, column()),
-                            new Plot(row() - 1, column()),
-                            new Plot(row(), column() + 1),
-                            new Plot(row(), column() - 1)
-                    )
+            return getNeighbours()
+                    .stream()
                     .filter(plot -> plot.isWithinBounds(rows, columns))
                     .toList();
+        }
+
+        private List<Plot> getNeighbours() {
+            return List.of(
+                    new Plot(row() + 1, column()),
+                    new Plot(row() - 1, column()),
+                    new Plot(row(), column() + 1),
+                    new Plot(row(), column() - 1)
+            );
         }
 
         public boolean isWithinBounds(int rows, int columns) {
--- src/test/java/ch/ebynaqon/aoc/aoc23/StepCounterTest.java
+++ src/test/java/ch/ebynaqon/aoc/aoc23/StepCounterTest.java
@@ -1,7 +1,16 @@
 package ch.ebynaqon.aoc.aoc23;
 
+import ch.ebynaqon.aoc.aoc23.StepCounter.Garden;
+import ch.ebynaqon.aoc.aoc23.StepCounter.Plot;
 import ch.ebynaqon.aoc.aoc23.helper.TestHelper;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,7 +31,8 @@ class StepCounterTest {
                 ...........
                 """.trim();
 
-        var actual = StepCounter.numberOfReachablePlots(input, 6);
+        Plot startingPlot = StepCounter.findStartingPlot(input);
+        var actual = new StepCounter(input).numberOfReachablePlots(6, startingPlot, true, false);
 
         assertThat(actual).isEqualTo(16);
     }
@@ -31,8 +41,105 @@ class StepCounterTest {
     void getNumberOfReachableSpacesForPart1() {
         var input = TestHelper.readInput("/day21-step-counter.txt").trim();
 
-        var actual = StepCounter.numberOfReachablePlots(input, 64);
+        Plot startingPlot = StepCounter.findStartingPlot(input);
+        var actual = new StepCounter(input).numberOfReachablePlots(64, startingPlot, true, false);
 
         assertThat(actual).isEqualTo(3574);
     }
+
+    @ParameterizedTest
+    @MethodSource("startingPlots")
+    void getNumberOfReachableSpacesFromEntryPoints(Plot startingPlot) {
+        var input = TestHelper.readInput("/day21-step-counter.txt").trim();
+
+        int stepGoal = startingPlot.row() == 65 || startingPlot.column() == 65 ? 130 + 66 : 2 * 130;
+        var actual = new StepCounter(input).numberOfReachablePlots(stepGoal, startingPlot, true, false);
+
+        assertThat(actual).isEqualTo((startingPlot.row() + startingPlot.column()) % 2 == 0
+                ? 7327 : 7336);
+    }
+
+    public static Stream<Arguments> startingPlots() {
+        return Stream.of(
+                Arguments.of(new Plot(0, 0)),
+                Arguments.of(new Plot(0, 65)),
+                Arguments.of(new Plot(0, 130)),
+                Arguments.of(new Plot(65, 0)),
+                Arguments.of(new Plot(65, 65)),
+                Arguments.of(new Plot(65, 130)),
+                Arguments.of(new Plot(130, 0)),
+                Arguments.of(new Plot(130, 65)),
+                Arguments.of(new Plot(130, 130))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("gardenAndStartingPosition")
+    void startingPositionForGarden(Garden garden, Plot startingPlot) {
+        assertThat(garden.startingPlot()).isEqualTo(startingPlot);
+    }
+
+    public static Stream<Arguments> gardenAndStartingPosition() {
+        return Stream.of(
+                Arguments.of(new Garden(-1, -1), new Plot(130, 130)),
+                Arguments.of(new Garden(-1, 0), new Plot(130, 65)),
+                Arguments.of(new Garden(-1, 1), new Plot(130, 0)),
+                Arguments.of(new Garden(0, -1), new Plot(65, 130)),
+                Arguments.of(new Garden(0, 0), new Plot(65, 65)),
+                Arguments.of(new Garden(0, 1), new Plot(65, 0)),
+                Arguments.of(new Garden(1, -1), new Plot(0, 130)),
+                Arguments.of(new Garden(1, 0), new Plot(0, 65)),
+                Arguments.of(new Garden(1, 1), new Plot(0, 0))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("gardenAndDistanceFromStart")
+    void distanceFromStartForGarden(Garden garden, int distance) {
+        assertThat(garden.distanceFromStart()).isEqualTo(distance);
+    }
+
+    public static Stream<Arguments> gardenAndDistanceFromStart() {
+        return Stream.of(
+                Arguments.of(new Garden(-1, -1), 2 * 66),
+                Arguments.of(new Garden(-1, 0), 66),
+                Arguments.of(new Garden(-1, 1), 2 * 66),
+                Arguments.of(new Garden(0, -1), 66),
+                Arguments.of(new Garden(0, 0), 0),
+                Arguments.of(new Garden(0, 1), 66),
+                Arguments.of(new Garden(1, -1), 2 * 66),
+                Arguments.of(new Garden(1, 0), 66),
+                Arguments.of(new Garden(1, 1), 2 * 66),
+                Arguments.of(new Garden(2, 3), (66 + 131) + (66 + 2 * 131))
+        );
+    }
+
+    @Test
+    void numberOfReachablePlotsInInfiniteGarden() {
+        var input = TestHelper.readInput("/day21-step-counter.txt").trim();
+
+        var actual = new StepCounter(input).numberOfReachablePlotsInInfiniteGarden(26501365);
+
+        assertThat(actual).isEqualTo(600090522932119L);
+    }
+
+    @ParameterizedTest
+    @MethodSource("stepGoalsForComparison")
+    @Disabled
+    void comparingAlgorithms(int stepGoal) {
+        var input = TestHelper.readInput("/day21-step-counter.txt").trim();
+
+        Plot startingPlot = StepCounter.findStartingPlot(input);
+        var actual = new StepCounter(input).numberOfReachablePlotsInInfiniteGarden(stepGoal);
+        var expected = new StepCounter(input).numberOfReachablePlots(stepGoal, startingPlot, stepGoal % 2 == 0, true);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    public static Stream<Arguments> stepGoalsForComparison() {
+        return IntStream.range(0, 7)
+                .flatMap(i -> IntStream.range(i * 131 + 60, i * 131 + 70))
+                .mapToObj(Arguments::of);
+    }
+
 }
